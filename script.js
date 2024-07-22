1386d122ad2377527717b56805b21d3a309b46ae
Daniel Vasquez <dwvasquez97@gmail.com>
2024-07-22 10:45:20 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Assign a variable to equal 10 using any arithmetic */
+/* Assign a variable to equal 40 using any arithmetic */
+/* Assign a variable to equal 39 using any arithmetic */
+/* 10 - 40 - 39 */
+/* Create a string containing the message below */
+
+// You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+
+/* Create an alert containing the message as well as the combination */
+
+const key1 = 5 + 5;
+const key2 = 100 - 60;
+const key3 = 39 * 1;
+const combination = `${key1} - ${key2} - ${key3}`;
+const message = "You have received this message because you have been chosen to open an important vault. \nHere is the secret combination: "
+
+alert(message + combination);
+
+
